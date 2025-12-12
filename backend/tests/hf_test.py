a62c8f1d9795593232432c896744f16404b8ab83
KarimaChami <karimachami24@gmail.com>
2025-12-12 14:49:58 +0100

    dockerisation

--- backend/tests/hf_test.py
+++ backend/tests/hf_test.py
@@ -5,21 +5,14 @@ def test_mock_hf(mocker):
     input_text = "Le marché boursier a connu une hausse significative aujourd'hui en raison des nouvelles économiques positives"
     expected_hf_output = [{"label": "Marketing", "score": 0.95},{"label": "Finance", "score": 0.25}]
 
-    # Création d'un faux objet réponse
     fake_response = mocker.Mock()
     fake_response.status_code = 200
     fake_response.json.return_value = expected_hf_output
 
     # Mock de requests.post
-    mocker.patch(
-        "app.hf_client.requests.post",
-        return_value=fake_response
-    )
-
-    # Appel de la fonction réelle
+    mocker.patch("app.hf_client.requests.post",return_value=fake_response)
+    
     result = zero_shot_classify(input_text)
-
-    # Assertions
     assert result == expected_hf_output
     assert result[0] == expected_hf_output[0]
     
